d20921489372123eb47758dcdf01279f32149d4e
sujin <ifuleav_e@naver.com>
2024-03-05 17:56:25 +0900

    반복문 예제1

---
+++ src/Loop/Ex/LoopEx_For.java
@@ -0,0 +1,13 @@
+package Loop.Ex;
+
+public class LoopEx_For {
+    public static void main(String[] args) {
+
+        //for 문
+      for (int count = 1 ; count <= 10 ; count++){
+          System.out.println(count);
+      }
+
+
+    }
+}
---
+++ src/Loop/Ex/LoopEx_While.java
@@ -0,0 +1,14 @@
+package Loop.Ex;
+
+public class LoopEx_While {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        while (count < 10){
+            count++;
+            System.out.println(count);
+        }
+
+    }
+}
