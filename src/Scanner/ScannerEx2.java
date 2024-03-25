5a214ffc0f912717b3c465b6fc008fef8d761c9e
sujin <ifuleav_e@naver.com>
2024-03-25 17:34:21 +0900

    커밋

---
+++ src/Scanner/ScannerEx2.java
@@ -0,0 +1,7 @@
+package Scanner;
+
+public class ScannerEx2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Scope/Casting2.java
@@ -0,0 +1,12 @@
+package Scope;
+
+public class Casting2 {
+    public static void main(String[] args) {
+        double doubleValue = 1.5;
+        int intValue=  0;
+
+        //intValue = doubleValue;
+        intValue = (int) doubleValue;
+        System.out.println(intValue);
+    }
+}
