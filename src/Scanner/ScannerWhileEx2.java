04aa01d056715fb03db814a80855abdc6a683db5
sujin <ifuleav_e@naver.com>
2024-03-26 17:19:52 +0900

    훈련

---
+++ src/Scanner/ScannerWhileEx2.java
@@ -0,0 +1,28 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true){
+            System.out.println("상품의 가격을 입력하세요 (-1을 입력하면 종료): ");
+            int price = input.nextInt();
+
+            if(price == -1){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            System.out.println("상품의 가격을 입력하세요 (-1을 입력하면 종료): ");
+            int quantity = input.nextInt();
+            input.nextLine();
+
+            int total = price*quantity;
+            System.out.println("총 비용 : " + total);
+
+        }
+
+    }
+}
